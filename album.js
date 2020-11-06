9fcb643de9490495c7b857af36118361a021365a
elinoza <hillcakmak@gmail.com>
2020-11-06 18:47:23 +0300

    dd

---
+++ album.js
@@ -0,0 +1,75 @@
+
+
+window.onload = () => {
+    loadTracks()
+
+  };
+
+
+
+
+
+ let fetchTracks =(endp) => {
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/179080312" + endp, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "49f4e1e876msh90846b28b1ca30fp193c58jsn17d62173eae9",
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+         console.log(response.json);
+          return response.json();
+        }
+      })
+      .then((body) => {
+        console.log(body);
+       
+     
+      })
+  
+      .catch((err) => {
+        alert("AN ERROR HAS OCCURRED  " + err);
+        console.error(err);
+      });
+    }
+
+
+    const loadTracks = function()  {
+       
+            const parsedUrl = new URL(window.location.href);        
+            let endp= parsedUrl.searchParams.get("id");
+            const  albumTitle=document.querySelector('#album-title');
+            const albumArtist=document.querySelector('#album-artist');
+            const albumCover=document.querySelector('#album-img');
+            const trackList=document.querySelector('#trackl-col');
+           
+            fetchTracks(endp, (body) => {
+              console.log(body.data);
+              albumTitle.innerText = body.data.album.title;
+              albumArtist.innerText = body.data.artist.name;
+              albumCover.innerText =  body.data.album.cover_medium;             
+                body.data.forEach((element) => {
+                        let trackUl=document.createElement('ul')
+                        trackUl.className="tracklist pr-3 p-1"
+                        trackUl.innerHTML += ` <li class="d-flex">
+                        <a href="#">
+                        <i class="align-self-start fas fa-music mr-2"></i>
+
+                        <div class="d-inline align-self-start">
+                            <p>${element.album.title}</p>
+                            <p class="subtitle"><a href="#"> ${element.artist.name} </a></p>
+                        </div>
+                        <p class="d-inline subtitle ml-auto">1:13</p>
+                        </a>
+                        </li>` 
+                        trackList.appendChild(trackUl)                 
+                 });                          
+            });
+    }
+       
+      
+  
+  
+
---
+++ home.js
@@ -0,0 +1,114 @@
+window.onload = () => {
+  window.location.href.includes("index") ? loadAlbums(1) : "";
+};
+
+let fetchAlbums = (endp, data) => {
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/" + endp, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "49f4e1e876msh90846b28b1ca30fp193c58jsn17d62173eae9",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        // console.log(response.json);
+        return response.json();
+      }
+    })
+    .then((body) => {
+      // console.log(body);
+      data(body);
+    })
+
+    .catch((err) => {
+      alert("AN ERROR HAS OCCURRED  " + err);
+      console.error(err);
+    });
+};
+const loadAlbums = (option) => {
+  let endpoint = [],
+    pages = [];
+  if (option === 1) {
+    endpoint = [
+      ["jazz", "classical", "R&B"],
+      ["techno", "indie", "pop"],
+      ["rap", "classical", "R&B"],
+      ["jazz", "classical", "R&B"],
+      ["jazz", "classical", "R&B"],
+    ];
+
+    pages = ["#trending", "#contents", "#mooDandGenres", "#newReleases", "#discover"];
+  } else {
+    endpoint = [[option]];
+    pages = ["#Results"];
+  }
+
+  let endp;
+  pages.forEach((page, index) => {
+    endpoint[index].forEach((el) => {
+      endp = "search?q=" + el;
+      fetchAlbums(endp, (body) => {
+        console.log(body.data);
+        let cards = document.createElement("div");
+        cards.classList.add("row");
+        const destination = document.querySelector(page);
+        const header = document.createElement("h3");
+        header.innerText = el.toUpperCase();
+        body.data.forEach((element, index) => {
+          index < 10
+            ? (cards.innerHTML += `<div class="col-12 col-md-4  col-lg-2 px-2">
+        <div class="card bg-transparent border-0 text-center">
+        <a href='/Album Page.html?id=${element.album.id}'>
+          <img
+            src="${element.album.cover}"
+            class="card-img-top"
+            alt="..."
+          />
+          </a>
+          <div class="card-body">
+            <button class="card-text  btn text-truncate text-white" onclick="playmusic(${element.album.id})" style="max-width: 150px;">${element.album.title}</button>
+            <a class="card-text text-truncate text-muted"  href='./artist.html?id=${element.artist.id}'  style="max-width: 150px;">${element.artist.name}</a>
+          </div>
+        </div>
+      </div>`)
+            : "";
+        });
+        destination.appendChild(header);
+        destination.appendChild(cards);
+      });
+    });
+  });
+};
+
+/*******************FOR THE SEARCH PAGE************************ */
+const searchAlbums = () => {
+  const search = document.querySelector("#search").value;
+  const cards = document.querySelectorAll(".card");
+  const h3s = document.querySelectorAll("h3");
+  cards.forEach((card) => card.remove());
+  h3s.forEach((h3) => h3.remove());
+  loadAlbums(search);
+};
+const filterAlbums = (event) => {
+  const search = event.target.value;
+  search.length > 2 ? searchAlbums() : "";
+};
+
+/**********FOOOTER PLAY MUSIC************/
+const x = document.getElementById("myAudio");
+function playAudio() {
+  x.play();
+}
+
+function changevolume(amount) {
+  const audioobject = document.getElementsByTagName("audio")[0];
+  audioobject.volume = amount;
+}
+const playmusic = (id) => {
+  id = "/album/" + id;
+  fetchAlbums(id, (body) => {
+    track = body.tracks.data[0];
+    console.log(track);
+  });
+};
